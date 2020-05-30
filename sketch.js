1ba090596800e21e49de8d12e34fed3a772525e4
happysmaran <smaranvallabhaneni@gmail.com>
2020-05-30 17:57:42 +0530

    FireBase

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 var ball;
+var database, pos;
 
 function setup(){
     createCanvas(500,500);
+
+    database=firebase.database();
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    database.ref("ball/position").on("value",readPos, showErr);
 }
 
 function draw(){
@@ -24,6 +30,18 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref("ball/position").set({
+        x:pos.x+x,
+        y:pos.y+y
+    })
+}
+
+function readPos(data){
+    pos=data.val();
+    ball.x=pos.x;
+    ball.y=pos.y;
+}
+
+function showErr(){
+    console.log("Error OOCCUURREEDD");
 }
