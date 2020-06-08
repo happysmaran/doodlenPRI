430faaf00c3e6db854361c41efeee085c1658daa
happysmaran <smaranvallabhaneni@gmail.com>
2020-06-08 19:22:59 +0530

    hgdg

--- sketch.js
+++ sketch.js
@@ -1,14 +1,17 @@
-var drawingball;
-var database, pos, piece;
+var drawingball, i;
+var database, pos, piece, button;
 
 var trajectory=[];
 
 function setup(){
-    createCanvas(500,500);
+    createCanvas(4000,4000);
 
     database=firebase.database();
 
     piece=loadImage("piece.png");
+
+    button=createButton("clearAll");
+    
 
     drawingball = createSprite(250,250,10,10);
     drawingball.shapeColor = "red";
@@ -23,19 +26,26 @@ function draw(){
     var position = [drawingball.x, drawingball.y];
     trajectory.push(position);
 
-    for(var i=0; i<trajectory.length; i++){
+
+    for(i=0; i<trajectory.length; i++){
         image(piece, trajectory[i][0], trajectory[i][1]);
     }
+
+    button.mousePressed(function(){
+        trajectory=[];
+    });
 
     database.ref("drawingball").set({
         drawings:trajectory
     })
+
+    
 }
 
 function changePosition(){
     database.ref("drawingball/position").set({
-        x:mouseX,
-        y:mouseY
+        x:trajectory[i][0],
+        y:trajectory[i][1]
     })
 }
 
